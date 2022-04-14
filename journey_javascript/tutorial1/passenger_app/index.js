899d46af5d7268323c8e6451fa7cfda6564f6d8d
Vrajs16 <Vrajs16108@gmail.com>
2022-04-13 21:41:08 -0400

    Finished Passenger_app

--- journey_javascript/tutorial1/passenger_app/index.js
+++ journey_javascript/tutorial1/passenger_app/index.js
@@ -1,17 +1,22 @@
-
 let countEl = document.getElementById("count-el");
+let saveEL = document.getElementById("save-el");
 
 let count = 0;
 
 function increment() {
     count += 1;
     countEl.innerText = count;
-    console.log(count);
 }
 
-function decrement() {
-    count -= 1;
-    countEl.innerText = count;
-    console.log(count);
+function save() {
+    saveEL.textContent += count + " - ";
+    count = 0;
+    countEl.textContent = count;
 }
 
+// let welcomeEl = document.getElementById("welcome-el");
+// let myname = "Vraj";
+// let greeting = "Welcome back!"
+// welcomeEl.innerText = greeting + " " + myname;
+
+// welcomeEl.innerText += "👋";
