c92e1a92b512c7fb952f03c905148ffb85880a66
Vraj Shah <vrajs16108@gmail.com>
2022-06-22 16:24:59 -0400

    Bloc Tutorial

---
+++ journey_flutter/bloc_state_management_tutorial/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:bloc_state_management_tutorial/blocs/color/color_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ColorBloc(),
+      child: MaterialApp(
+        home: BlocBuilder<ColorBloc, ColorState>(
+          builder: (context, state) {
+            return Container(
+              color: state.color,
+              child: FloatingActionButton(
+                onPressed: () {
+                  context.read<ColorBloc>().add(ChangeColorEvent());
+                  debugPrint("Color Will Change");
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
