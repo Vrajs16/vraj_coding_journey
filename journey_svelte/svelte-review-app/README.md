a19ba41054a134ff3df26c9b06fca868a3731c8d
Vraj Shah <vrajs16108@gmail.com>
2022-06-30 16:43:15 -0400

    Finished Svelte Review App

---
+++ journey_svelte/svelte-review-app/README.md
@@ -0,0 +1,7 @@
+# Svelte Review App
+
+### Deployment
+
+### Link
+
+[Tutorial Link](https://www.youtube.com/watch?v=3TVy6GdtNuQ)
---
+++ journey_svelte/svelte-review-app/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import FeedbackForm from "./components/FeedbackForm.svelte";
+  import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
+  // const addFeedback = (e) => {
+  //   const newFeedback = e.detail;
+  //   feedback = [newFeedback, ...feedback];
+  // };
+  // const deleteFeedback = (e) => {
+  //   const itemId = e.detail;
+  //   feedback = feedback.filter((item) => item.id != itemId);
+  // };
+</script>
+
+<main class="container">
+  <FeedbackForm />
+  <!-- on:add-feedback={addFeedback} -->
+  <FeedbackStats />
+  <FeedbackList />
+  <!-- on:delete-feedback={deleteFeedback}-->
+</main>
