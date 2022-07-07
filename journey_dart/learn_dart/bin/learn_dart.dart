6ae4efd78d9bca59f0865866db1ec503533ca2c4
Vraj Shah <vrajs16108@gmail.com>
2022-07-07 10:13:03 -0400

    Learning Dart

---
+++ journey_dart/learn_dart/bin/learn_dart.dart
@@ -0,0 +1,7 @@
+import 'package:learn_dart/learn_dart.dart' as learn_dart;
+
+void main(List<String> arguments) {
+  int $count = 3;
+  print("${$count}");
+  print('Hello world: ${learn_dart.calculate()}!');
+}
